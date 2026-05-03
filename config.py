01b9037e4a8391cc64a1d131ea125e84c46d79c3
gamerpatel <gbot03558@gmail.com>
2026-05-03 11:00:30 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,24 +1,24 @@
 import os
 from os import getenv
 # ---------------R---------------------------------
-API_ID = int(os.environ.get("API_ID", "20369373"))
+API_ID = int(os.environ.get("API_ID", "20831039"))
 # ------------------------------------------------
-API_HASH = os.environ.get("API_HASH", "0d53cc7f978163fed3263be5cbb20ab0")
+API_HASH = os.environ.get("API_HASH", "8075567864:AAHHCKNboLYuN4gq3_RjS_6cnzGtvZC46kw")
 # ----------------D--------------------------------
 BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
 # -----------------A-------------------------------
-BOT_USERNAME = os.environ.get("Masterextratorraonebot")
+BOT_USERNAME = os.environ.get("any_uploder_bot")
 # ------------------X------------------------------
-OWNER_ID = int(os.environ.get("OWNER_ID", "5680454765"))
+OWNER_ID = int(os.environ.get("OWNER_ID", "7597020624"))
 # ------------------X------------------------------
-CREATOR_ID = int(os.environ.get("CREATOR_ID", "5680454765"))
-LOG_CHANNEL_ID = int(os.environ.get("LOG_CHANNEL_ID", "-1002585176866"))
+CREATOR_ID = int(os.environ.get("CREATOR_ID", "7597020624"))
+LOG_CHANNEL_ID = int(os.environ.get("LOG_CHANNEL_ID", "-1003874496367"))
 
 
-SUDO_USERS = list(map(int, getenv("SUDO_USERS", "5680454765").split()))
+SUDO_USERS = list(map(int, getenv("SUDO_USERS", "7597020624").split()))
 # ------------------------------------------------
-CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1002585176866"))
+CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1003874496367"))
 # ------------------------------------------------
-MONGO_URL = os.environ.get("MONGO_URL", "")
+MONGO_URL = os.environ.get("MONGO_URL", "mongodb+srv://gbot11007:gbot11007@cluster0.qnknxxh.mongodb.net/?appName=Cluster0")
 # -----------------------------------------------
-PREMIUM_LOGS = int(os.environ.get("PREMIUM_LOGS", "-1002585176866"))
+PREMIUM_LOGS = int(os.environ.get("PREMIUM_LOGS", "-1003874496367"))
